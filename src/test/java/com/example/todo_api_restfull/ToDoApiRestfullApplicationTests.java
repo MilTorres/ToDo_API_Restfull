c352ee5d2b054b5887795dea2a233818880ef1bc
Milton Torres <Miltontorres018@gmail.com>
2023-05-22 11:29:53 -0600

    Initial commit

---
+++ src/test/java/com/example/todo_api_restfull/ToDoApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_api_restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiRestfullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
